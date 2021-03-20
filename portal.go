c661389ee91621a96dd537b5177e0499a588c8ec
lla <lla4u@hotmail.com>
2021-03-20 11:51:29 +0100

    Fixed few issues

---
+++ portal.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os/exec"
+	"time"
+)
+
+const backupPath string = "/tmp/backup"
+
+// Backup function
+// return influxdb database backup file as zip (todo)
+func Backup(w http.ResponseWriter, r *http.Request) {
+
+	// influxd backup -portable <path-to-backup>
+	cmd := exec.Command("influxd", "backup", "-portable", backupPath)
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal("Backup Error: ", err)
+	}
+
+	url := "http://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png"
+
+	timeout := time.Duration(5) * time.Second
+	transport := &http.Transport{
+		ResponseHeaderTimeout: timeout,
+		Dial: func(network, addr string) (net.Conn, error) {
+			return net.DialTimeout(network, addr, timeout)
+		},
+		DisableKeepAlives: true,
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	//copy the relevant headers. If you want to preserve the downloaded file name, extract it with go's url parser.
+	w.Header().Set("Content-Disposition", "attachment; filename=Wiki.png")
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", r.Header.Get("Content-Length"))
+
+	//stream the body to the client without fully loading it into memory
+	io.Copy(w, resp.Body)
+}
+
+func portal() {
+	http.HandleFunc("/backup", Backup)
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("HTTP Error", err)
+	}
+}
